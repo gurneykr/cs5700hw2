44f51a3c1ea9c9666e1aa9fe861746e628c236c3
Krista Gurney <gurneykri@gmail.com>
2018-09-17 20:31:31 -0600

    Got the TrackerServer to get the information from the simulation!!

--- src/TrackerServer.java
+++ src/TrackerServer.java
@@ -1,5 +1,32 @@
+import java.net.SocketException;
+
 public class TrackerServer {
+    private Communicator communicator;
+
+
     public static void main(String[] args){
+        TrackerServer server = new TrackerServer();
+        try{
+            server.start();
+        }catch(SocketException e){
+            System.err.println("Failed to start communicator");
+        }
+
+    }
+
+    public TrackerServer(){
         System.out.println("Hello Tracker Server");
     }
+    public void start()throws SocketException{
+        System.out.println("Starting Tracker Server");
+
+        DummyMessageProcessor messageProcessor = new DummyMessageProcessor("Dummy message processor");
+
+        communicator = new Communicator(12000);
+        communicator.setProcessor(messageProcessor);
+        communicator.start();
+
+
+    }
+
 }
